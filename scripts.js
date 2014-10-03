d01458f3cf5433808dd9a42a791e43802225a9d7
Stephen Young <StephenJayYoung@gmail.com>
2014-10-03 15:54:32 -0700

    this is not yet correct, but getting there

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+// @function reverseConcatArray
+// @param {Array} array - the array to be reversed
+// @returns {Array} newArray - the reversed array that was passed to the function
+
+var array = ['Steve', 'Austin', 'Kali', 'Andrea', 'Leslie'];
+
+var reverseConcatArray = function(array) {
+	array.forEach.length;
+	return reverseConcatArray;
+};
+
+array.prototype.of = function() {
+	var newArray = reverseConcatArray;
+	return newArray;
+};
+
+console.log(newArray);
